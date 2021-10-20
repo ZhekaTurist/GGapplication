56d12a4b5a80937773bcb6872fe62294ce769f0f
ZhekaTurist <eugen.coffee@yandex.ru>
2021-10-20 15:09:06 +0800

    add new text with string element. change new color for this test

---
+++ app/src/main/java/zheka/turist/ggapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package zheka.turist.ggapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
